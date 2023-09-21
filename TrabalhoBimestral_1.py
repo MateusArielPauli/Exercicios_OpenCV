b14ce2e53bfc5be4e9a39282a377c37c961821e0
Mateus Ariel Pauli <62779446+MateusArielPauli@users.noreply.github.com>
2023-09-21 11:48:57 -0300

    versaoBeta

--- TrabalhoBimestral_1.py
+++ TrabalhoBimestral_1.py
@@ -20,15 +20,10 @@ class PdiApp(QMainWindow):
         super().__init__()
         self.selected_image = None
         self.setWindowTitle("Trabalho Bimestral")
-        self.setGeometry(100, 100, 300, 500)
+        self.setGeometry(100, 100, 300, 300)
 
         # Layout principal
         main_layout = QVBoxLayout()
-
-        open_button = QPushButton("Abrir Imagem", self)
-        open_button.clicked.connect(self.open_image)
-        main_layout.addWidget(open_button)
-
 
         # Botões para ações
         convert_button = QPushButton("Converção de cor: HLS", self)
@@ -43,6 +38,21 @@ class PdiApp(QMainWindow):
         laplace_button.clicked.connect(self.apply_laplace_edge_detection)
         main_layout.addWidget(laplace_button)
 
+        binary_button = QPushButton("Binarizar imagem: Threshould", self)
+        binary_button.clicked.connect(self.apply_binarization)
+        main_layout.addWidget(binary_button)
+
+        erosion_button = QPushButton("Morfologia Matemática: Erosão", self)
+        erosion_button.clicked.connect(self.apply_erosion)
+        main_layout.addWidget(erosion_button)
+
+        open_button = QPushButton("Abrir Imagem", self)
+        open_button.clicked.connect(self.open_image)
+        main_layout.addWidget(open_button)
+
+        reset_button = QPushButton("Reverter para Original", self)
+        reset_button.clicked.connect(self.reset_image)
+        main_layout.addWidget(reset_button)
     
         # Label para exibir a imagem
         self.image_label = QLabel(self)
@@ -67,6 +77,26 @@ class PdiApp(QMainWindow):
             edges = cv2.Laplacian(self.selected_image, cv2.CV_64F)
             edges = cv2.convertScaleAbs(edges)
             self.display_image(edges)
+
+    def apply_binarization(self):
+        if self.selected_image is not None:
+            # Aplicar binarização com um limiar (threshold)
+            gray_image = cv2.cvtColor(self.selected_image, cv2.COLOR_BGR2GRAY)
+            _, binary_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+            self.display_image(binary_image)
+
+    def apply_erosion(self):
+        if self.selected_image is not None:
+            # Aplicar operação de erosão
+            gray_image = cv2.cvtColor(self.selected_image, cv2.COLOR_BGR2GRAY)
+            _, binary_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+            kernel = np.ones((5, 5), np.uint8)
+            erosion_image = cv2.erode(binary_image, kernel, iterations=1)
+            self.display_image(erosion_image)
+
+    def reset_image(self):
+        if self.selected_image is not None:
+            self.display_image(self.selected_image)
 
     def display_image(self, image_copy):
         if image_copy is not None:
